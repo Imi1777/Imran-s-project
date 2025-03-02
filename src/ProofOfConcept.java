54a33013b52a9dd1a132f8b6e13fd344874e7d73
Imi1777 <Kingsoccer160@gmail.com>
2025-03-02 17:43:41 -0500

    update

--- src/ProofOfConcept.java
+++ src/ProofOfConcept.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
 import components.sequence.Sequence;
 import components.sequence.Sequence1L;
+import components.sequence.Sequence2L;
 
 public class ProofOfConcept {
 
@@ -7,11 +11,19 @@ public class ProofOfConcept {
     public record Game(String team1, String team2, int score, int score2) {
     }
 
+    public record lineUp(String teamNames, String teamNames2,
+            List<String> players1, List<String> player2) {
+
+    }
+
     Sequence<Game> games;
+
+    Sequence<lineUp> players;
 
     // TODO - how do we construct a ScoreTracker
     public ProofOfConcept() {
         this.games = new Sequence1L<>();
+        this.players = new Sequence2L<>();
     }
 
     // TODO - Implement kernels
@@ -40,6 +52,17 @@ public class ProofOfConcept {
         ProofOfConcept gamesList = new ProofOfConcept();
         gamesList.addGame(new Game("Manchester United", "Chelsea", 17, 1));
         System.out.println(gamesList.showAllGames());
+
+        //creat the player teams
+        List<String> manUtdPlayers = Arrays
+                .asList("Rashford, Onana, Bruno, Amad");
+        List<String> chelseaPlayers = Arrays
+                .asList("Palmer, Nkunku,Reece, Jackson");
+
+        lineUp gameLinUp = new lineUp("Manchester United", "Chelsea",
+                manUtdPlayers, chelseaPlayers);
+
+        System.out.println(gameLinUp);
     }
 
 }
