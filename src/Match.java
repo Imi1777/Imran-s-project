c2b71cf7190377d96f43eb0ea117169f9a1603fa
Imi1777 <Kingsoccer160@gmail.com>
2025-04-05 23:53:11 -0400

    update

--- src/Match.java
+++ src/Match.java
@@ -1,5 +1,10 @@
 import java.util.List;
 
+
+/**
+ * this represents a soccer match between two teams.
+ * this also includes the teams, their players, and the match's score.
+ */
 public record Match(String team1, String team2, List<String> team1Players,
         List<String> team2Players, int team1Score, int team2Score) {
 }
